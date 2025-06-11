6f4886f70fea33a3488e3203854f25f998a9c661
SrLLamaDev <srllamadev@gmail.com>
2025-06-11 17:21:03 -0400

    asd

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Bienvenido a la escuela</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
 </head>
-<body>
-    <h1>Welcome to My Website</h1>
-    <p>This is a simple HTML template.</p>  
+<body class="body-home">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
