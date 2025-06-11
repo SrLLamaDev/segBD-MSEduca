7599676db4282b9a0d1feb9f4d7b5af1f8094f53
SrLLamaDev <srllamadev@gmail.com>
2025-06-11 17:49:51 -0400

    test

--- index.php
+++ index.php
@@ -5,8 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido a la escuela</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="body-home">
+    <div class="black-fill">
+        <div class="container">
+
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+    <h1>Bienvenido a la escuela</h1>
 </body>
 </html>
