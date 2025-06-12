3985a798e4944b759646bfdf061db45b50719d37
SrLLamaDev <srllamadev@gmail.com>
2025-06-12 12:23:03 -0400

    asd

--- index.php
+++ index.php
@@ -30,12 +30,12 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">
+                                <a class="nav-link" href="#about">
                                     Acerca de
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">
+                                <a class="nav-link" href="#contact">
                                     Contacto
                                 </a>
                             </li>
@@ -61,8 +61,46 @@
         <!--asd-->
             <section class="welcome-text d-flex justify-content-center align-items-center flex-column">
                 <img src="img/logo.webp">
-                <h4>Bienvenido a las escuela</h4>
+                <h4>Bienvenido a las E</h4>
                 <p>Esta es una plataforma de gestión escolar que te permite administrar estudiantes, profesores y cursos de manera eficiente.</p>	
+            </section>
+
+            <section class="welcome-text d-flex justify-content-center align-items-center flex-column">
+                <div class="card mb-3 card-1">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/logo.webp" class="img-fluid rounded-start" >
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">About Us</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Y School</small></p>
+                            </div>
+                        </div>
+                    </div>
+			    </div>                
+            </section>
+
+            <section id="contact"
+                 class="d-flex justify-content-center align-items-center flex-column">
+        	    <form>
+                    <h3>Contactanos</h3>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Direccion de email</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">AAAAAAAAA</div>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Nombre</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Apellidos</label>
+                        <textarea class="form-control" rows="4"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
             </section>
         </div>
     </div>
